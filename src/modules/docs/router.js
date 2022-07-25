aaf00f05ee79a1a12434f3596ad36d05a8149179
Yeison9713 <yeison9713@gmail.com>
2022-07-25 14:10:42 -0500

    add Lib to gitignore

--- src/modules/docs/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const { HandleDocs } = require('./handle')
-
-app.get('/files', HandleDocs.prototype.get_files)
-app.get('/textextract-pdf', HandleDocs.prototype.get_text_pdf)
-
-module.exports = app;
